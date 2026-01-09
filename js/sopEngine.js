afac2c601f7006e3bab8fbd0c7eb67878b72bea5
himshim26 <97538806+himshim@users.noreply.github.com>
2026-01-09 16:21:38 +0530

    Update sopEngine.js

--- js/sopEngine.js
+++ js/sopEngine.js
@@ -38,9 +38,9 @@ function renderInspectionFormat(d) {
     <h2>STANDARD OPERATING PROCEDURE</h2>
 
     <table>
-      <tr><td><b>Institution</b></td><td>${d.institute.name || ""}</td></tr>
-      <tr><td><b>Department</b></td><td>${d.institute.dept || ""}</td></tr>
-      <tr><td><b>SOP Title</b></td><td>${d.meta.title || ""}</td></tr>
+      <tr><td><b>Institution</b></td><td>${escapeHTML(d.institute.name || "")}</td></tr>
+      <tr><td><b>Department</b></td><td>${escapeHTML(d.institute.dept || "")}</td></tr>
+      <tr><td><b>SOP Title</b></td><td>${escapeHTML(d.meta.title || "")}</td></tr>
     </table>
 
     <table>
@@ -56,29 +56,29 @@ function renderInspectionFormat(d) {
     <hr>
   `;
 
-  html += `<h4>${sec++}.0 PURPOSE</h4><p>${d.sections.purpose || ""}</p>`;
-  html += `<h4>${sec++}.0 SCOPE</h4><p>${d.sections.scope || ""}</p>`;
+  html += `<h4>\( {sec++}.0 PURPOSE</h4><p> \){escapeHTML(d.sections.purpose || "")}</p>`;
+  html += `<h4>\( {sec++}.0 SCOPE</h4><p> \){escapeHTML(d.sections.scope || "")}</p>`;
 
   html += `
     <h4>${sec++}.0 RESPONSIBILITY</h4>
-    <p>Laboratory In-charge, faculty members, and trained users.</p>
+    <p>${escapeHTML(d.sections.responsibility || "Laboratory In-charge, faculty members, and trained users.")}</p>
   `;
 
   html += `<h4>${sec}.0 PROCEDURE</h4>`;
   d.sections.procedure.forEach((s, i) => {
-    html += `<p>${sec}.${i + 1} ${s}</p>`;
+    html += `<p>\( {sec}. \){i + 1} ${escapeHTML(s)}</p>`;
   });
   sec++;
 
-  html += `<h4>${sec}.0 PRECAUTIONS</h4><p>${d.sections.precautions || ""}</p>`;
+  html += `<h4>\( {sec}.0 PRECAUTIONS</h4><p> \){escapeHTML(d.sections.precautions || "")}</p>`;
 
   html += `
     <hr>
     <table>
       <tr>
-        <td><b>Prepared By</b><br>${d.authority.prepared || ""}<br>Date: ___</td>
-        <td><b>Checked By</b><br>${d.authority.checked || ""}<br>Date: ___</td>
-        <td><b>Authorized By</b><br>${d.authority.approved || ""}<br>Date: ___</td>
+        <td><b>Prepared By</b><br>${escapeHTML(d.authority.prepared || "")}<br>Date: ___</td>
+        <td><b>Checked By</b><br>${escapeHTML(d.authority.checked || "")}<br>Date: ___</td>
+        <td><b>Authorized By</b><br>${escapeHTML(d.authority.approved || "")}<br>Date: ___</td>
       </tr>
     </table>
 
@@ -96,30 +96,31 @@ function renderBeginnerFormat(d) {
   let html = "";
 
   html += `
-    <h2>${d.institute.name || "INSTITUTE NAME"}</h2>
-    <h4>${d.institute.dept || "DEPARTMENT"}</h4>
+    <h2>${escapeHTML(d.institute.name || "INSTITUTE NAME")}</h2>
+    <h4>${escapeHTML(d.institute.dept || "DEPARTMENT")}</h4>
     <h3>STANDARD OPERATING PROCEDURE</h3>
-    <h4>${d.meta.title || "SOP TITLE"}</h4>
+    <h4>${escapeHTML(d.meta.title || "SOP TITLE")}</h4>
     <hr>
   `;
 
-  html += `<h4>Purpose</h4><p>${d.sections.purpose || ""}</p>`;
-  html += `<h4>Scope</h4><p>${d.sections.scope || ""}</p>`;
+  html += `<h4>Purpose</h4><p>${escapeHTML(d.sections.purpose || "")}</p>`;
+  html += `<h4>Scope</h4><p>${escapeHTML(d.sections.scope || "")}</p>`;
+  html += `<h4>Responsibility</h4><p>${escapeHTML(d.sections.responsibility || "")}</p>`;
 
   html += `<h4>Procedure</h4>`;
   d.sections.procedure.forEach((s, i) => {
-    html += `<p>${i + 1}. ${s}</p>`;
+    html += `<p>${i + 1}. ${escapeHTML(s)}</p>`;
   });
 
-  html += `<h4>Precautions</h4><p>${d.sections.precautions || ""}</p>`;
+  html += `<h4>Precautions</h4><p>${escapeHTML(d.sections.precautions || "")}</p>`;
 
   html += `
     <hr>
     <table>
       <tr>
-        <td><b>Prepared By</b><br>${d.authority.prepared || ""}</td>
-        <td><b>Checked By</b><br>${d.authority.checked || ""}</td>
-        <td><b>Authorized By</b><br>${d.authority.approved || ""}</td>
+        <td><b>Prepared By</b><br>${escapeHTML(d.authority.prepared || "")}</td>
+        <td><b>Checked By</b><br>${escapeHTML(d.authority.checked || "")}</td>
+        <td><b>Authorized By</b><br>${escapeHTML(d.authority.approved || "")}</td>
       </tr>
     </table>
   `;
