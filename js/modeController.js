c632f7b9c9b3c41296bfdf7ba935c27ff5bc1925
himshim26 <97538806+himshim@users.noreply.github.com>
2026-01-09 09:54:31 +0530

    Call renderPreview on mode change

--- js/modeController.js
+++ js/modeController.js
@@ -5,5 +5,6 @@ document.querySelectorAll("input[name='mode']").forEach(r => {
     currentMode = e.target.value;
     document.getElementById("customBox").classList.toggle("hidden", currentMode !== "custom");
     document.getElementById("predefinedBox").classList.toggle("hidden", currentMode === "custom");
+    renderPreview();
   });
 });
