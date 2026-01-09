fe6e93b05c64cf29165b0e1ae79f31f89765fea0
himshim26 <97538806+himshim@users.noreply.github.com>
2026-01-09 16:20:26 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,15 @@
 function today() {
   return new Date().toISOString().split("T")[0];
 }
+
+function escapeHTML(str) {
+  return str.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#039;');
+}
+
+function debounce(func, delay = 300) {
+  let timeout;
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), delay);
+  };
+}
