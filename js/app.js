73ecbfbcf8fef8aa2f2b0cc24fbcd90c8beca810
himshim26 <97538806+himshim@users.noreply.github.com>
2026-01-09 16:22:25 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  loadDefaultSOP();
+  // loadDefaultSOP() moved to uiController
+  const debouncedRender = debounce(renderPreview);
   document.querySelectorAll("input, textarea").forEach(el =>
-    el.addEventListener("input", renderPreview)
+    el.addEventListener("input", debouncedRender)
   );
 });
